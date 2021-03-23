d302e7e18599b8e48a12735da7d922e7cec8d908
enya <ryuusei.y@icloud.com>
2021-03-23 16:45:23 +0800

    first commit

---
+++ src/views/Home.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="home">
+    <input type="text" v-model="addressid">
+    <button @click="value">住所自動入力</button>
+    <p>address:{{addressin}}</p>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+export default {
+  data(){
+    return{
+      addressid:"",
+      addressdata:"",
+    }
+  },
+  methods: {
+    async value(addressin){
+          let address = await axios.get(`https://apis.postcode-jp.com/api/v4/${this.addressid}/addressdata?callback`)
+          addressin = JSON.stringify(address)
+    }
+  },
+
+}
+</script>
